403ccf3196f8ac3b4c7d28c58afa92aff7ae54c3
muhmmdakmal <muhmmdakmal.69@gmail.com>
2024-09-16 23:56:21 +0700

    Menyelesaikan Tugas 3

--- main/models.py
+++ main/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import uuid
+import datetime
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
     description = models.TextField()
+    time = models.DateField(default=datetime.date.today)# Menampilkan waktu
+    rating_barang = models.IntegerField() # Rating untuk barang
+
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
+    products_entries = Product.objects.all()
     context = {
         'name' : 'Converse Chuck Taylor All Star Vulcanized Hi X Off-White',
         'image_url': 'C:/Users/Lenovo/OneDrive/Desktop/Kuliah/PBP/Solely-sneakers-shop/main/templates/ConverseOW.jpg',
@@ -8,7 +13,34 @@ def show_main(request):
         'description': 'Seri ini, yang dirancang oleh Virgil Abloh dari Off-White dan Nike, menggabungkan ketiga brand untuk versi baru yang menarik dari Converse Chuck Taylor klasik. Sepasang sepatu dari seri "Ghosting" ini hadir dalam warna putih, kerucut, dan biru es. Mereka memiliki bahan yang berbeda di seluruh bagian, serta sol tembus pandang biru es, tali pengikat merah, merek hitam, dan bagian atas tembus pandang yang direkonstruksi untuk sesuai dengan temanya. Tersedia bersama dengan koleksi Off-White x Nike "Ghosting" lainnya, akan dirilis pada 1 November 2017.',
         'name_creator':'Muhammad Akmal Abdul Halim',
         'npm_creator':'2306245125',
-        'class_creator':'PBP-D'
+        'class_creator':'PBP-D',
+        'products_entries' : products_entries
     }
 
     return render(request, "main.html", context)
+
+def create_rating(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_rating.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
