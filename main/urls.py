2ba794eb576bebc73e7851708e8ceee47bc60750
muhmmdakmal <muhmmdakmal.69@gmail.com>
2024-11-17 14:56:44 +0700

    Setup Untuk Tugas 9

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product, create_product_flutter 
 from main.views import add_product_ajax
 app_name = 'main'
 
@@ -18,4 +18,5 @@ urlpatterns = [
     path('edit-product/<uuid:id>/', edit_product, name='edit_product'),
     path('delete/<uuid:id>/', delete_product, name='delete_product'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
